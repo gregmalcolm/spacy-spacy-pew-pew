14ba4b822731bdbd6b10ac91e3fdf4b2fa89f59f
Greg <greg@gregmalcolm.com>
2023-12-27 14:50:44 -0500

    Minor adjustments

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var acceleration := 10.0
-@export var max_speed := 350.0
+@export var max_speed := 300.0
 @export var rotation_speed := 250.0
 
 func _process(_delta):
@@ -15,9 +15,9 @@ func _physics_process(delta):
 	velocity = velocity.limit_length(max_speed)
 	
 	if Input.is_action_pressed("rotate_right"):
-		rotate(deg_to_rad(rotation_speed*delta))
+		rotate(deg_to_rad(rotation_speed * delta))
 	if Input.is_action_pressed("rotate_left"):
-		rotate(deg_to_rad(-rotation_speed*delta))
+		rotate(deg_to_rad(-rotation_speed * delta))
 	
 	if input_vector.y == 0:
 		velocity = velocity.move_toward(Vector2.ZERO, 3)
