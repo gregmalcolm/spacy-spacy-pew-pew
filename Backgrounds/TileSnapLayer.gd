b6542a1d32da5825f358661ea83fb41c797e1608
Greg <greg@gregmalcolm.com>
2023-12-29 00:17:57 -0500

    Stopped messing starfield offset idea for now

--- Backgrounds/TileSnapLayer.gd
+++ Backgrounds/TileSnapLayer.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 @export_subgroup("Motion")
-@export var offset: Vector2 = Vector2(0,0)
 @export var sizing: Vector2 = Vector2(0,0)
 
 func _process(_delta):
@@ -16,5 +15,5 @@ func _realign_child_node(child):
 	var camera = get_viewport().get_camera_2d()
 	var camera_position = camera.get_screen_center_position()
 
-	child.position.x = offset.x + (floor(camera_position.x/sizing.x) * sizing.x)
-	child.position.y = offset.y + floor(camera_position.y/sizing.y) * sizing.y
+	child.position.x = floor(camera_position.x/sizing.x) * sizing.x
+	child.position.y = floor(camera_position.y/sizing.y) * sizing.y
